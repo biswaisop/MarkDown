4e43696635064311671eb1023ee14e22b3b769e5
Biswadip mandal <95963808+biswaisop@users.noreply.github.com>
2024-10-15 00:43:09 +0530

    navbarpadfix

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -12,10 +12,10 @@ import {
 
 const Navbar = () => {
   return (
-    <nav className="p-4 bg-background/50 sticky top-0 border-b backdrop:blur">
-      <div className="container mx-auto flex pb-6 justify-between items-center">
+    <nav className=" bg-background/50 sticky top-0 border-b md:pb-3 backdrop:blur">
+      <div className="container mx-auto flex   justify-between items-center">
         <Link href="/">
-          <div className="text-black text-2xl font-bold">BiLoG</div>
+          <div className="text-black pt-4 pb-4 pl-0 text-3xl font-bold">BiLoG</div>
         </Link>
         <div className="hidden md:flex space-x-4 items-center">
           <Link href="/" className="text-black">
@@ -59,10 +59,27 @@ const Navbar = () => {
 
             <SheetContent>
               <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetTitle className="pb-4">BiLog</SheetTitle>
                 <SheetDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
+                  <div className="flex flex-col gap-6">
+                    <Link href="/" className="text-black">
+                      Home
+                    </Link>
+                    <Link href="/about" className="text-black">
+                      About
+                    </Link>
+                    <Link href="/contact" className="text-black">
+                      Contact
+                    </Link>
+                    <div>
+                      <Button className="mx-1" variant="outline">
+                        Login
+                      </Button>
+                      <Button className="mx-1" variant="outline">
+                        Sign Up
+                      </Button>
+                    </div>
+                  </div>
                 </SheetDescription>
               </SheetHeader>
             </SheetContent>
